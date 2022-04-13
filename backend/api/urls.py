9b9a9f94ef461f1369ab96a9610ede8b38644190
Dmitriy <77408415+themasterid@users.noreply.github.com>
2022-04-13 10:32:36 +0500

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
 from api.views import (AddAndDeleteSubscribe, AddDeleteFavoriteRecipe,
                        AddDeleteShoppingCart, IngredientsViewSet,
                        RecipesViewSet, TagsViewSet, UsersViewSet, set_password)
