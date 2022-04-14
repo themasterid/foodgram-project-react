22136c24cddfbd784ac5e04995327d2608927309
themasterid <thebrootos@gmail.com>
2022-04-14 21:42:20 +0500

    deploy 14.04.2022

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,10 +7,10 @@ BASE_DIR = os.path.dirname(
     os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = (
-    os.getenv('SECRET_KEY'),
+    'SECRET_KEY',
     'my_mega_secret_code_ilz@4zqj=rq&agdol^##zgl9(vs')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default='True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get(
     'ALLOWED_HOSTS', default='localhost').split(', ')
@@ -66,19 +66,24 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv(
-            'DB_ENGINE', default='django.db.backends.postgresql'),
+            'DB_ENGINE',
+            default='django.db.backends.postgresql'),
         'NAME': os.getenv(
-            'DB_NAME'),
+            'DB_NAME',
+            default='postgres'),
         'USER': os.getenv(
-            'POSTGRES_USER'),
+            'POSTGRES_USER',
+            default='postgres'),
         'PASSWORD': os.getenv(
-            'POSTGRES_PASSWORD'),
+            'POSTGRES_PASSWORD',
+            default='postgres'),
         'HOST': os.getenv(
-            'DB_HOST'),
+            'DB_HOST',
+            default='db'),
         'PORT': os.getenv(
-            'DB_PORT')
-    }
-}
+            'DB_PORT',
+            default='5432'),
+    }}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -105,10 +110,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
