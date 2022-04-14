9dc515a07515b51a0feb20a64bc366badecdf4a7
themasterid <thebrootos@gmail.com>
2022-04-15 03:21:26 +0500

    fix password

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (AddAndDeleteSubscribe, AddDeleteFavoriteRecipe,
-                       AddDeleteShoppingCart, IngredientsViewSet,
-                       RecipesViewSet, TagsViewSet, AuthToken, UsersViewSet,
-                       set_password)
+                       AddDeleteShoppingCart, AuthToken, IngredientsViewSet,
+                       RecipesViewSet, TagsViewSet, UsersViewSet, set_password)
 
 app_name = 'api'
 
