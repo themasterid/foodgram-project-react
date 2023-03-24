c4a3ccde60d975df69e42d083907c76b5f3be463
Sourcery AI <>
2023-03-24 12:34:34 +0000

    'Refactored by Sourcery'

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -49,9 +49,11 @@ class GetIsSubscribedMixin:
 
     def get_is_subscribed(self, obj):
         user = self.context['request'].user
-        if not user.is_authenticated:
-            return False
-        return user.follower.filter(author=obj).exists()
+        return (
+            user.follower.filter(author=obj).exists()
+            if user.is_authenticated
+            else False
+        )
 
 
 class UserListSerializer(
